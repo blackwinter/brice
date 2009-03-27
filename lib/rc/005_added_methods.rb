ab1a1219bde6537d3d2fa31deff69cc37ec9ee64
Jens Wille <jens.wille@uni-koeln.de>
2009-03-27 12:04:49 +0100

    AddedMethods is now a standalone project.

--- lib/rc/005_added_methods.rb
+++ lib/rc/005_added_methods.rb
@@ -1,23 +1,22 @@
-# Load Util::AddedMethods[http://prometheus.rubyforge.org/ruby-nuggets/classes/Util/AddedMethods.html]
-# from ruby-nuggets[http://prometheus.rubyforge.org/ruby-nuggets/] if one (or both) of the following
-# environment variables has been set:
+# Load AddedMethods[http://added_methods.rubyforge.org/] if one
+# (or both) of the following environment variables has been set:
 #
 # <tt>WATCH_FOR_ADDED_METHODS</tt>::    Regular expression or <tt>true</tt>
 # <tt>WATCH_FOR_ADDED_METHODS_IN</tt>:: Space-delimited list of class names
 
-brice 'added_methods' => 'nuggets/util/added_methods' do |config|
+brice 'added_methods' do |config|
 
   regexp  = ENV['WATCH_FOR_ADDED_METHODS']
   klasses = ENV['WATCH_FOR_ADDED_METHODS_IN']
 
   if regexp || klasses
     if regexp == 'true'
-      Util::AddedMethods.init
+      AddedMethods.init
     else
       regexp  = Regexp.new(regexp || '')
       klasses = (klasses || '').split
 
-      Util::AddedMethods.init(regexp, klasses)
+      AddedMethods.init(regexp, klasses)
     end
   end
 
