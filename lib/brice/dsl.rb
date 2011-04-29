81884b6825841b66b578e064c9300b1067475607
Jens Wille <jens.wille@uni-koeln.de>
2011-04-29 14:51:47 +0200

    Switched license to Affero GPL.

--- lib/brice/dsl.rb
+++ lib/brice/dsl.rb
@@ -3,23 +3,23 @@
 #                                                                             #
 # A component of brice, the extra cool IRb goodness donator                   #
 #                                                                             #
-# Copyright (C) 2008 Jens Wille                                               #
+# Copyright (C) 2008-2011 Jens Wille                                          #
 #                                                                             #
 # Authors:                                                                    #
 #     Jens Wille <jens.wille@uni-koeln.de>                                    #
 #                                                                             #
 # brice is free software: you can redistribute it and/or modify it under the  #
-# terms of the GNU General Public License as published by the Free Software   #
-# Foundation, either version 3 of the License, or (at your option) any later  #
-# version.                                                                    #
+# terms of the GNU Affero General Public License as published by the Free     #
+# Software Foundation, either version 3 of the License, or (at your option)   #
+# any later version.                                                          #
 #                                                                             #
 # brice is distributed in the hope that it will be useful, but WITHOUT ANY    #
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   #
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more       #
-# details.                                                                    #
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for     #
+# more details.                                                               #
 #                                                                             #
-# You should have received a copy of the GNU General Public License along     #
-# with brice. If not, see <http://www.gnu.org/licenses/>.                     #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with brice. If not, see <http://www.gnu.org/licenses/>.               #
 #                                                                             #
 ###############################################################################
 #++
